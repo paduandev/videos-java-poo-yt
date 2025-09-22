23c1bbb4e9c4db9574775959888fae2ddb56908a
Emerson Paduan <emerson@paduan.dev.br>
2025-09-22 20:00:51 -0300

    video 12

---
+++ video12/App.java
@@ -0,0 +1,18 @@
+public class App {
+    public static void main(String[] args) {
+
+        Guerreiro g1 = new Guerreiro();
+        Guerreiro g2 = new Guerreiro();
+        Guerreiro g3 = new Guerreiro();
+        Guerreiro g4 = new Guerreiro();
+        Guerreiro g5 = new Guerreiro();
+
+        System.out.println(Guerreiro.getCont());
+        System.out.println(Guerreiro.getCont());
+        System.out.println(Guerreiro.getCont());
+        System.out.println(Guerreiro.getCont());
+        System.out.println(Guerreiro.getCont());
+        System.out.println(Guerreiro.getCont());
+
+    }
+}
---
+++ video12/Guerreiro.java
@@ -0,0 +1,11 @@
+public class Guerreiro {
+    private static int cont;
+
+    public Guerreiro() {
+        cont++;
+    }
+
+    public static int getCont() {
+        return cont;
+    }
+}
